5f0da093e42e7d17efdb209be0cba2b3e99f33e3
hukasigi <hukasigiteya2@gmail.com>
2026-01-21 17:21:12 +0900

    raw

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,9 @@ const int8_t  PIM_ROTARY_A   = 26;
 const int8_t  PIN_ROTARY_B   = 27;
 volatile long rolls          = 0;
 volatile bool value_rotary_b = 0;
-static int    cnt            = 0;
+volatile long pos            = 0;
+volatile long speed          = 0;
+volatile long lastPos        = 0;
 
 // 一秒周期のタイマー割り込み カウントリセット
 // 立ち上がったときのB相のを見て回転方向を読み取る
@@ -13,6 +15,10 @@ void IRAM_ATTR detect_turn_a() {
     value_rotary_b = digitalRead(PIN_ROTARY_B);
     value_rotary_b ? rolls++ : rolls--;
 }
+
+hw_timer_t* timer = NULL;
+
+// timer = timerBegin(0,80,true);
 
 void setup() {
     Serial.begin(115200);
