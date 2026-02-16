6f59b68dda1c47429d741e56f6867f5370f2c093
hukasigi <hukasigiteya2@gmail.com>
2026-02-17 01:42:39 +0900

    fix:calculation

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,20 @@ const uint8_t PIN_ROTARY_A = 26;
 const uint8_t PIN_ROTARY_B = 27;
 const uint8_t PIN_PWM      = 25;
 const uint8_t PIN_DIR      = 33;
+const uint8_t PWM_CHANNEL  = 0;
+
+const double INTEGRAL_MAX = 500.;
+const double INTEGRAL_MIN = -500.;
+
+const uint16_t FREQUENCY       = 20000;
+const uint8_t  RESOLUTION_BITS = 8;
+
+// 制御周期
+const uint16_t CONTROL_CYCLE = 100000;
 
 // 目標位置
-const int16_t target = 1000;
+const int16_t angle  = 200;
+int           target = map(angle, 0, 360, 0, 2024);
 
 const double KP = 0.3;
 const double KI = 0.002;
@@ -14,8 +25,8 @@ const double KD = 0.005;
 
 volatile long pos            = 0;
 volatile bool value_rotary_b = 0;
-static long   lastPos        = 0;
 double        integral       = 0;
+double        prev_error     = 0;
 
 hw_timer_t*  timer    = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
@@ -23,9 +34,9 @@ portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 // 立ち上がったときのB相のを見て回転方向を読み取る
 // HIGHだったらcw(),LOWだったらccw()のように
 void IRAM_ATTR detect_turn_a() {
-    bool b = gpio_get_level((gpio_num_t)PIN_ROTARY_B);
+    bool b_in = gpio_get_level((gpio_num_t)PIN_ROTARY_B);
     portENTER_CRITICAL_ISR(&timerMux);
-    b ? pos-- : pos++;
+    b_in ? pos-- : pos++;
     portEXIT_CRITICAL_ISR(&timerMux);
 }
 
@@ -37,43 +48,39 @@ void setup() {
 
     attachInterrupt(PIN_ROTARY_A, detect_turn_a, FALLING);
 
-    ledcAttachPin(PIN_PWM, 0);
-    ledcSetup(0, 20000, 8);
+    ledcAttachPin(PIN_PWM, PWM_CHANNEL);
+    ledcSetup(PWM_CHANNEL, FREQUENCY, RESOLUTION_BITS);
 }
 
 void loop() {
-    static unsigned long last  = micros();
-    unsigned long        now_t = micros();
-    if (now_t - last >= 10000) { // 制御周期10ms
-        // 経過時間、秒単位。
-        double DT = (now_t - last) / 1e6;
-        last      = now_t;
+    static unsigned long prev_time = micros();
+    unsigned long        now_time  = micros();
+    if (now_time - prev_time >= CONTROL_CYCLE) {
+        double dt = (now_time - prev_time) / 1e6;
+        prev_time = now_time;
 
+        /* NOTE: 割り込みが発生したときにposの値は違う値になってしまうので、
+        別の変数にコピーして使用する*/
         long pos_now;
         portENTER_CRITICAL(&timerMux);
         pos_now = pos;
         portEXIT_CRITICAL(&timerMux);
 
-        /*posをそのまま使ってしまうと、ここで割り込みが発生したときに
-        誤差が狂ってしまう*/
         int error = target - pos_now;
 
-        double vel        = (pos_now - lastPos) / DT;
-        double derivative = -vel;
+        double derivative = (error - prev_error) / dt;
+        prev_error        = error;
 
-        lastPos = pos_now;
-
-        integral = constrain(integral, -500, 500);
+        integral += error * dt;
+        // I項によるオーバーフローを防ぐため
+        integral = constrain(integral, INTEGRAL_MIN, INTEGRAL_MAX);
 
         double control = KP * error + KI * integral + KD * derivative;
 
-        bool dir = (control > 0);
-        digitalWrite(PIN_DIR, dir);
+        uint8_t pwm = constrain(abs(control), 0, 255);
 
-        int pwm = abs(control);
-        pwm     = constrain(pwm, 0, 255);
-
-        ledcWrite(0, pwm);
+        digitalWrite(PIN_DIR, control > 0 ? HIGH : LOW);
+        ledcWrite(PWM_CHANNEL, pwm);
 
         Serial.print("pos  ");
         Serial.println(pos_now);
