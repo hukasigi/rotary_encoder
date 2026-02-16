fbcf18a28114882f1bd4d482e47ac7786d734c6e
hukasigi <hukasigiteya2@gmail.com>
2026-02-17 00:35:10 +0900

    fix:calculation

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,24 @@ const int8_t PIN_ROTARY_B = 27;
 const int8_t PIN_PWM      = 25;
 const int8_t PIN_DIR      = 33;
 
-const double  RPM_FIL_PER = 0.7;
-const double  RPM_RAW_PER = 0.3;
-const double  TARGET_RPM  = 200.;
-const int16_t RESOLUTION  = 2048;
+const double  TARGET_RPM = 200.;
+const int16_t RESOLUTION = 2048;
 
-const double KP = 2.50;
-const double KI = 1.00;
+const double KP = 0.7;
+const double KI = 0.30;
 const double KD = 0.0005;
 
-int    pwm_out  = 0;
-double rpm_f    = 0;
-double integral = 0;
+int    du        = 0;
+double output    = 0;
+double integral  = 0;
+double deriv     = 0;
+double prop      = 0;
+double pre_prop  = 0;
+double pre_error = 0;
 
 volatile long rolls          = 0;
 volatile long rolls_is       = 0;
 volatile bool value_rotary_b = 0;
-volatile long pos            = 0;
 volatile long speed          = 0;
 
 static unsigned long last    = 0;
@@ -73,42 +74,29 @@ void loop() {
     unsigned long now_t = micros();
 
     if (now_t - last >= 10000) { // 10ms
-        double DT = (now_t - last) / 1e6;
+        double dt = (now_t - last) / 1e6;
         last      = now_t;
         long r;
         portENTER_CRITICAL(&timerMux);
         r = rolls_is;
         portEXIT_CRITICAL(&timerMux);
 
-        double rpm = (double)r / RESOLUTION * 60. / DT;
-
-        // 回転数にフィルタをかけてなめらかにする
-        // rpm_f = 0.7 × 昔の値 + 0.3 × 今の値
-        rpm_f = rpm_f * RPM_FIL_PER + rpm * RPM_RAW_PER;
+        double rpm = (double)r / RESOLUTION * 60. / dt;
 
         // 目標値 - 実測値でエラーを出す
-        double error = TARGET_RPM - rpm_f;
+        double error = TARGET_RPM - rpm;
 
-        // 回転数を時間で微分
-        // errorの微分だと目標rpmが変わったら急にD項が暴れる
-        double derivative = -(rpm_f - lastRpm) / DT;
-        lastRpm           = rpm_f;
+        prop  = error - pre_error;
+        deriv = prop - pre_prop;
+        du    = KP * prop + KI * error * dt + KD * deriv;
+        output += du;
+        output = constrain(output, 0, 255);
 
-        double control_raw = KP * error + KI * integral + KD * derivative;
-        if (abs(control_raw) < 255) { // 飽和してない時だけ積分
-            // ズレが長時間つづいた分を足す
-            integral += error * DT;
-        }
+        ledcWrite(0, output);
 
-        integral       = constrain(integral, -500, 500);
-        double control = KP * error + KI * integral + KD * derivative;
+        Serial.printf("RPM: %.1f  PWM: %d ERROR : %.1f\n", rpm, output, error);
 
-        bool dir = (control <= 0);
-        digitalWrite(PIN_DIR, dir);
-        pwm_out = constrain(abs(control), 0, 255);
-
-        ledcWrite(0, pwm_out);
-
-        Serial.printf("RPM: %.1f  PWM: %d ERROR : %.1f\n", rpm, pwm_out, error);
+        pre_error = error;
+        pre_prop  = prop;
     }
 }
