36c65083e8f73a84bd124f9f225a9bf1ba128ef2
hukasigi <hukasigiteya2@gmail.com>
2026-02-18 17:27:11 +0900

    fix:gain

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 const uint8_t PIN_ROTARY_A = 26;
 const uint8_t PIN_ROTARY_B = 27;
+const uint8_t PIN_ROTARY_Z = 32;
 const uint8_t PIN_PWM      = 25;
 const uint8_t PIN_DIR      = 33;
 const uint8_t PWM_CHANNEL  = 0;
@@ -13,15 +14,15 @@ const uint16_t FREQUENCY       = 20000;
 const uint8_t  RESOLUTION_BITS = 8;
 
 // 制御周期
-const uint16_t CONTROL_CYCLE = 100000;
+const uint16_t CONTROL_CYCLE = 2000;
 
 // 目標位置
 const int16_t angle  = 200;
 int           target = map(angle, 0, 360, 0, 2024);
 
-const double KP = 0.3;
-const double KI = 0.002;
-const double KD = 0.005;
+const double KP = 0.5;
+const double KI = 0.1;
+const double KD = 0.03;
 
 volatile long pos            = 0;
 volatile bool value_rotary_b = 0;
@@ -44,12 +45,30 @@ void setup() {
     Serial.begin(115200);
     pinMode(PIN_ROTARY_A, INPUT_PULLUP);
     pinMode(PIN_ROTARY_B, INPUT_PULLUP);
+    pinMode(PIN_ROTARY_Z, INPUT_PULLUP);
     pinMode(PIN_DIR, OUTPUT);
-
-    attachInterrupt(PIN_ROTARY_A, detect_turn_a, FALLING);
-
     ledcAttachPin(PIN_PWM, PWM_CHANNEL);
     ledcSetup(PWM_CHANNEL, FREQUENCY, RESOLUTION_BITS);
+
+    // 初期位置合わせ
+    bool prevZ = gpio_get_level((gpio_num_t)PIN_ROTARY_Z);
+
+    ledcWrite(PWM_CHANNEL, 30); // 低速が安全
+
+    while (1) {
+        bool nowZ = gpio_get_level((gpio_num_t)PIN_ROTARY_Z);
+
+        if (!prevZ && nowZ) { // 立ち上がり検出
+            break;
+        }
+
+        prevZ = nowZ;
+    }
+    ledcWrite(PWM_CHANNEL, 0);
+    pos = 0;
+    Serial.println("start");
+
+    attachInterrupt(PIN_ROTARY_A, detect_turn_a, FALLING);
 }
 
 void loop() {
